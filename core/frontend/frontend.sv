9e61317b5ff581699eea60e958a9a2d9bd434387
Maryamzarinnal <maryam.zarinnal@studio.unibo.it>
2026-03-19 13:32:19 +0100

    tc active use disabled

--- core/frontend/frontend.sv
+++ core/frontend/frontend.sv
@@ -837,10 +837,12 @@ module frontend
   localparam int unsigned TC_SAME_PC_REPLAY_CAP = 256;
   logic [15:0] tc_same_pc_replay_count_q;
 
-  assign tc_active_use = tc_active_hit
-                      && tc_trace_starts_ok
-                      && (tc_trace_next_pc != tc_trace_pcs[0])
-                      && !tc_feeding_q;
+//  assign tc_active_use = tc_active_hit
+//                      && tc_trace_starts_ok
+//                      && (tc_trace_next_pc != tc_trace_pcs[0])
+//                      && !tc_feeding_q;
+  assign tc_active_use = 1'b0;
+
 
 // pragma translate_off
   logic         tc_feeding_q_prev;
